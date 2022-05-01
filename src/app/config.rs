6fd2ab8e1506aa6e835e3973c4f1c08a342b95fa
Jan Keith Darunday <jkcdarunday@gmail.com>
2022-05-01 19:42:10 +0800

    Fix deprecated function usage in config and remove unnecessary use of rwlock

--- src/app/config.rs
+++ src/app/config.rs
@@ -1,16 +1,14 @@
-use std::sync::RwLock;
 use config::Config;
 
 lazy_static! {
-	pub static ref CONFIG: RwLock<Config> = RwLock::new({
-        let mut settings = Config::default();
-        settings.merge(config::File::with_name("config.toml")).unwrap();
-        settings.merge(config::Environment::with_prefix("APP")).unwrap();
-
-        settings
-    });
+	pub static ref CONFIG: Config =
+        Config::builder()
+            .add_source(config::File::with_name("config"))
+            .add_source(config::Environment::with_prefix("APP"))
+            .build()
+            .unwrap();
 }
 
 pub fn get<'a, T: serde::Deserialize<'a>>(key: &str) -> T{
-    CONFIG.read().unwrap().get::<T>(key).unwrap()
+    CONFIG.get::<T>(key).unwrap()
 }
