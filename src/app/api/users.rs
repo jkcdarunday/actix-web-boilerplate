808264edd9d5d0040f901ed065c8a6c9e6a98ee3
Jan Keith Darunday <jkcdarunday@gmail.com>
2023-08-03 05:11:41 +0800

    Fix table load now requiring a mutable connection

--- src/app/api/users.rs
+++ src/app/api/users.rs
@@ -12,12 +12,11 @@ pub async fn list(db_pool: web::Data<DbPool>) -> impl Responder {
         return HttpResponse::InternalServerError().body(format!("{:?}", e));
     }
 
-    let con = con_result.unwrap();
-    let query_result = web::block(move || users::table.load::<user::User>(&*con).unwrap()).await;
+    let mut con = con_result.unwrap();
+    let query_result = web::block(move || users::table.load::<user::User>(&mut *con).unwrap()).await;
     if let Err(e) = query_result {
         return HttpResponse::InternalServerError().body(format!("{:?}", e));
     }
 
     HttpResponse::Ok().json(query_result.unwrap())
 }
-
