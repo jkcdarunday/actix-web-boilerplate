9715f0d41864a18118877b95f41bdc6fe1d2257d
Jan Keith Darunday <jkcdarunday@gmail.com>
2023-08-30 09:03:38 +0800

    Add a delete user endpoint

--- src/app/api/users.rs
+++ src/app/api/users.rs
@@ -1,9 +1,12 @@
-use actix_web::{get, post, web, HttpResponse, Responder, Result};
+use actix_web::{delete, get, post, web, HttpResponse, Responder, Result};
+use actix_web_validator::Path;
 use diesel::insert_into;
 use diesel::prelude::*;
+use validator::Validate;
 
 use crate::app::error::AppError;
 use crate::app::lib::helpers::get_connection;
+use crate::app::lib::validators::user_id_exists;
 use crate::app::models::*;
 use crate::schema::users;
 
@@ -30,6 +33,23 @@ pub async fn create(user: web::Json<user::User>) -> Result<impl Responder, AppEr
         ))
         .execute(&mut *con)
         .map_err(|e| AppError::new(500).cause(e).message("Failed to create user"))?;
+
+    Ok(HttpResponse::Ok().json(query_result))
+}
+
+#[derive(Deserialize, Validate)]
+pub struct DeleteRequest {
+    #[validate(custom = "user_id_exists")]
+    id: i32,
+}
+
+#[delete("/users/{id}")]
+pub async fn delete(path: Path<DeleteRequest>) -> Result<impl Responder, AppError> {
+    let mut con = get_connection()?;
+
+    let query_result = diesel::delete(users::table.filter(users::id.eq(path.id)))
+        .execute(&mut *con)
+        .map_err(|e| AppError::new(500).cause(e).message("Failed to delete user"))?;
 
     Ok(HttpResponse::Ok().json(query_result))
 }
