1b85f5a456326c08a475ca2195390a9a59fd66fa
Jan Keith Darunday <jkcdarunday@gmail.com>
2023-08-30 09:03:12 +0800

    Add validator for checking if a user id exists

---
+++ src/app/lib/validators.rs
@@ -0,0 +1,20 @@
+use diesel::prelude::*;
+use diesel::QueryDsl;
+use validator::ValidationError;
+
+use crate::app::db::DB;
+use crate::app::models::user::User;
+use crate::schema::users;
+
+pub fn user_id_exists(id: i32) -> Result<(), ValidationError> {
+    let query_result = users::table
+        .filter(users::id.eq(id))
+        .load::<User>(&mut DB.get().unwrap())
+        .map_err(|_| ValidationError::new("User not found"))?;
+
+    if query_result.len() == 0 {
+        return Err(ValidationError::new("User not found"));
+    }
+
+    Ok(())
+}
